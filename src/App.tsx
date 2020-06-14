18c693a2c390c915f38664d05b308f120873b8ab
LostInTransistor <signorello.marco@gmail.com>
2020-06-14 13:33:22 +0200

    Simple websocket server/client

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {w3cwebsocket as websocket } from 'websocket';
 
-function App() {
+const serverUrl = 'ws://localhost:8999/';
+const client = new websocket(serverUrl);
+
+const App = () => {
+  const [message, setMessage] = React.useState<string>('');
+
+  React.useEffect(() => {
+   client.onopen =
+      () => {
+        console.log('Websocket connected!')
+      };
+    client.onmessage = (message) =>{
+      setMessage(message.data.toString());
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Websocket Client 
+      <p>Message is {message}</p>
     </div>
   );
 }
---
+++ websocket-server/readme.md
@@ -0,0 +1,7 @@
+Websocket server simple test
+
+// Compile
+1) ./node_modules/.bin/tsc  
+
+// Run server
+2) node ./dist/server/server.js
